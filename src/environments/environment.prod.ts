6b2fc9769a09981fb4b9f9391980e8995442f605
Kirill Kapralov <Kapralov1501@gmail.com>
2024-09-17 15:20:59 +0300

    environment changes 3

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  api: 'https://umber-sai/api/',
-  serverStaticPath: 'https://umber-sai/images/products/'
+  api: 'https://umber-sai.com/api/',
+  serverStaticPath: 'https://umber-sai.com/images/products/'
   
 };
